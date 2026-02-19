095be2948015717b193e34950b5ae614c3706826
Tanner24 <doson7714@gmail.com>
2026-02-19 17:41:08 +0700

    Tang gioi han chunk size

--- pos-client/vite.config.js
+++ pos-client/vite.config.js
@@ -18,5 +18,8 @@ export default defineConfig({
                 rewrite: (path) => path.replace(/^\/api/, '')
             }
         }
+    },
+    build: {
+        chunkSizeWarningLimit: 1000,
     }
 })
